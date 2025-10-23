530921c709ba0dce6c50919290593ff63aa42fa1
Cunxing <40293349+cxlet@users.noreply.github.com>
2025-10-23 10:18:45 +0800

    Initialize LxAuthCenterApplicatAdd main Spring Boot application classion with Spring Boot

---
+++ src/main/java/com/example/lxauthcenter/LxAuthCenterApplication.java
@@ -0,0 +1,11 @@
+package com.example.lxauthcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxAuthCenterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LxAuthCenterApplication.class, args);
+    }
+}
